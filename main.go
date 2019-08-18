c8a8fe9901f735fa8eb4f55e52bb815c76a5b44f
川崎開発退避 <kawasakikaihatsutaihi@kawasakijunnoMacBook.local>
2019-08-18 22:28:44 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import(
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"github.com/chat_server2/handler"
+	"github.com/chat_server2/router"
+)
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/hello", handler.MainPage())
+
+	go func() {
+		if err := e.Start(":1323"); err != nil {
+			e.Logger.Info("shutting down the server")
+		}
+	}()
+}
+
