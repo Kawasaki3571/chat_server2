0a845eb18719b2306ae40702b168791e939327e7
川崎開発退避 <kawasaki.blackemp@gmail.com>
2019-08-22 06:39:15 +0900

    sqlをどうしよう

--- db/databasehandle.go
+++ db/databasehandle.go
@@ -6,12 +6,27 @@ import (
    "fmt"
 )
 // マイグレーションツールが見つかり使い方を習得し次第消す
-func Migration() error{
-	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/test_db")
+func DatabaseInit() error {
+	db, err := sql.Open("mysql", "user=root dbname=test_db password=root sslmode=disable")
 	if err != nil{
 		return err
 	}
-	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS "messages" ("id" INTEGER, "text" VARCHAR(255), "created_at" DATETIME)`,)
+	return nil
+}
+
+func Migration() error{
+	// db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/test_db")
+	db, err := sql.Open("mysql", "user=root dbname=test_db password=root sslmode=disable")
+	if err != nil{
+		return err
+	}
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS "messages" (
+			"id" INTEGER, 
+			"text" VARCHAR(255), 
+			"created_at" DATETIME,
+			PRIMARY KEY ("id")
+		)`)
 	if err != nil{
 		return err
 	}
@@ -19,6 +34,7 @@ func Migration() error{
 }
 
 func main() {
+	DatabaseInit()
 	Migration()
 	defer fmt.Println("終了")
 }
