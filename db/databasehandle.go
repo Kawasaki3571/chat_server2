6b8fcb29a4182a057960c370016f04d77f48be9f
川崎開発退避 <kawasaki.blackemp@gmail.com>
2019-08-31 19:40:03 +0900

    余分を消す

--- db/databasehandle.go
+++ db/databasehandle.go
@@ -14,29 +14,29 @@ func DatabaseInit() error {
 	return nil
 }
 
-// メモリアドレス不一致でパニックを起こした
-func Migration() error{
-	// db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/test_db")
-	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/local_database")
-	if err != nil{
-		return err
-	}
-	query, err := db.Query(`
-		CREATE TABLE IF NOT EXISTS "messages" (
-			"id" INTEGER, 
-			"text" VARCHAR(255), 
-			"created_at" DATETIME,
-			PRIMARY KEY ("id")
-		)`)
-	defer query.Close()
-	if err != nil{
-		return err
-	}
-	return nil
-}
+// // メモリアドレス不一致でパニックを起こした
+// func Migration() error{
+// 	// db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/test_db")
+// 	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/local_database")
+// 	if err != nil{
+// 		return err
+// 	}
+// 	query, err := db.Query(`
+// 		CREATE TABLE IF NOT EXISTS "messages" (
+// 			"id" INTEGER, 
+// 			"text" VARCHAR(255), 
+// 			"created_at" DATETIME,
+// 			PRIMARY KEY ("id")
+// 		)`)
+// 	defer query.Close()
+// 	if err != nil{
+// 		return err
+// 	}
+// 	return nil
+// }
 
 func main() {
 	DatabaseInit()
-	Migration()
+	// Migration()
 	defer fmt.Println("終了")
 }
