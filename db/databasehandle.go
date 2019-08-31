23620c31813460316fa4b7857158d13aa15a8bf2
川崎開発退避 <kawasaki.blackemp@gmail.com>
2019-09-01 01:27:02 +0900

    sql

--- db/databasehandle.go
+++ db/databasehandle.go
@@ -4,6 +4,7 @@ import (
     "database/sql"
    _ "github.com/go-sql-driver/mysql"
    "fmt"
+   "time"
 )
 // マイグレーションツールが見つかり使い方を習得し次第消す
 func DatabaseInit() error {
@@ -34,9 +35,26 @@ func DatabaseInit() error {
 // 	}
 // 	return nil
 // }
+func Seed() error{
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/local_database")
+	defer db.Close()
+	if err != nil{
+		return err
+		fmt.Println("aaaaa!?")
+	}
+	_, err = db.Exec(`INSERT INTO messages VALUES ($1, $2, $3)`, 1, "test_message", time.Now())
+	if err != nil {
+		return err
+		fmt.Println("aaaaa!?")
+	}
+	// defer query.Close()
+	return nil
+}
 
 func main() {
 	DatabaseInit()
 	// Migration()
+	Seed()
 	defer fmt.Println("終了")
 }
+
